c11ac1f4161e0a07a2db90a49538645c696e2596
Shiomi25 <kayrgi205@gmail.com>
2025-08-07 11:01:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Guardar estado en localStorage
+document.querySelectorAll('input[type=checkbox]').forEach(input => {
+  const id = input.id;
+  input.checked = localStorage.getItem(id) === 'true';
+
+  input.addEventListener('change', () => {
+    localStorage.setItem(id, input.checked);
+  });
+});
 
